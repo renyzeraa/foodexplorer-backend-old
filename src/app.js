7d08d0c915dc6fc89a3dc329b2758a567766f7ad
Renan Silva <renansilvaytB@gmail.com>
2023-07-29 11:19:58 -0300

    fix env, auth and app

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {})
-console.log(`Server is running on port => ${PORT}`)
+const PORT = process.env.PORT || 3333
+app.listen(PORT, () => {
+  console.log(`Server is running on port => ${PORT}`)
+})
